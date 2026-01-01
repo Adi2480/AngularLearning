4721cfcb6429258f0a900b6dcc6f30134eb7ff3b
ADIROCKS@2480 <adityagaikwad248@gmail.com>
2026-01-01 22:34:37 +0530

    new task component changes

--- 01-starting-project/01-starting-project/src/app/app.component.ts
+++ 01-starting-project/01-starting-project/src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TaskComponent } from './task/task.component';
+import { TasksComponent } from './task/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TaskComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = "u1";
+  selectedUserId?:string;
 
   get selectedUser(){
     return this.users.find((user) => user.id === this.selectedUserId)!;
